08f5e113b86eeaa8e39d043ecb904ecdc845d643
ivanovit <i.ivanov.it@gmail.com>
2022-02-07 14:52:59 +0200

    docs: fix error handler descriptions

--- errors/error_handler.go
+++ errors/error_handler.go
@@ -1,6 +1,6 @@
 package errors
 
-// ErrorHandler is the interface used to create custom panic errors that occur during query execution
+// ErrorHandler is the interface used to transform errors that occur during query execution
 type ErrorHandler interface {
 	TransformError(error) error
 }
@@ -8,7 +8,7 @@ type ErrorHandler interface {
 // DefaultErrorHandler is the default ErrorHandler
 type DefaultErrorHandler struct{}
 
-// MakePanicError creates a new QueryError from a panic that occurred during execution
+// TransformError could transform a error to a new error(hidding any sensitive information, logging the error, etc.)
 func (h *DefaultErrorHandler) TransformError(e error) error {
 	return e
 }
