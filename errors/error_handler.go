a1f3aa244810e0c9f7bdd9b108b94e74ffbe0166
ivanovit <i.ivanov.it@gmail.com>
2022-02-08 13:40:27 +0200

    fix: typo

--- errors/error_handler.go
+++ errors/error_handler.go
@@ -8,7 +8,7 @@ type ErrorHandler interface {
 // DefaultErrorHandler is the default ErrorHandler
 type DefaultErrorHandler struct{}
 
-// TransformError could transform a error to a new error(hidding any sensitive information, logging the error, etc.)
+// TransformError could transform a error to a new error(hiding any sensitive information, logging the error, etc.)
 func (h *DefaultErrorHandler) TransformError(e error) error {
 	return e
 }
