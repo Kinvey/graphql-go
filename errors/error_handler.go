8044e2abf9a5d74dcb5aaa07d08c7f39ad90fd76
ivanovit <i.ivanov.it@gmail.com>
2022-02-22 14:55:56 +0200

    feat: pass ctx to error handler

--- errors/error_handler.go
+++ errors/error_handler.go
@@ -1,14 +1,16 @@
 package errors
 
+import "context"
+
 // ErrorHandler is the interface used to transform errors that occur during query execution
 type ErrorHandler interface {
-	TransformError(error) error
+	TransformError(context.Context, error) error
 }
 
 // DefaultErrorHandler is the default ErrorHandler
 type DefaultErrorHandler struct{}
 
 // TransformError could transform a error to a new error(hiding any sensitive information, logging the error, etc.)
-func (h *DefaultErrorHandler) TransformError(e error) error {
+func (h *DefaultErrorHandler) TransformError(ctx context.Context, e error) error {
 	return e
 }
